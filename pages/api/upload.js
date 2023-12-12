caafa8960510fe625c27cda8b04759e1a471f384
alex <alexey.sinelnik2015@gmail.com>
2023-12-12 13:05:49 +0200

    Remove Google auth

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -2,14 +2,12 @@ import multiparty from "multiparty";
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import fs from "fs";
 import mime from "mime-types";
-import { isAdminRequest } from "./auth/[...nextauth]";
 import { mongooseConnect } from "../../lib/mongoose";
 
 const bucketName = "shop-admin-page";
 
 export default async function handle(req, res) {
     await mongooseConnect();
-    await isAdminRequest(req, res);
     const form = new multiparty.Form();
 
     const { fields, files } = await new Promise((resolve, reject) => {
